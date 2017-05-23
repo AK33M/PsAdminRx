3b5209d8ba391850ec8fa52faeeb0dfb902778f5
AK33M <akeemtaiwo@outlook.com>
2017-05-23 15:24:42 +0100

    Create Manage Course Form

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -1,24 +1,34 @@
 import React, {PropTypes} from 'react'
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import CourseForm from './CourseForm';
 import * as courseAction from '../../actions/courseAction';
 
 class ManageCoursePage extends React.Component {
 	constructor(props, context) {
 		super(props, context);
 
+		this.state = {
+			course: Object.assign({}, props.course),
+			errors: {}
+		};
 	}
 	render() {
 		return (
-			<h1>Manage Course</h1>
+			<CourseForm course={this.state.course}
+			 						errors={this.state.errors}
+									allAuthors={[]}/>
 		);
 	}
 }
 
-ManageCoursePage.PropTypes = {};
+ManageCoursePage.PropTypes = {
+	course: PropTypes.object.isRequired
+};
 
 function mapStateToProps(state, ownProps) {
-	return {state: state};
+	let course = { id:'', watchHref: '', title: '', authorId: '', length: '', category: ''};
+	return {course: course};
 }
 
 function mapDispatchToProps(dispatch) {
