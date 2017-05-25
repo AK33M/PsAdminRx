0acad4c94489030a291ed3029b6fd539ec35df06
Akeem Taiwo (Work) <akeem.taiwo@dataireland.ie>
2017-05-25 11:16:34 +0100

    author route, new authorpage

---
+++ src/components/author/AuthorsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AuthorsPage extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>Authors</h1>
+      </div>
+    );
+  }
+}
+
+export default AuthorsPage;
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
+import AuthorsPage from './components/author/AuthorsPage';
 import CoursesPage from './components/course/CoursesPage';
 import ManageCoursePage from './components/course/ManageCoursePage'; //eslint-disable-line import/no-named-as-default
 
@@ -13,5 +14,6 @@ export default(
 		<Route path="course/:id" component={ManageCoursePage}/>
     <Route path="course" component={ManageCoursePage}/>
 		<Route path="courses" component={CoursesPage}/>
+		<Route path="authors" component={AuthorsPage}/>
 	</Route>
 );
